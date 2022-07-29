e268bc5752426487f85a47eb2e8c754f1c300fad
bbora <>
2022-07-29 14:23:41 +0900

    2022.07.29 - 프로젝트 생성

---
+++ dietaryMadicineWithSwiftUI/dietaryMadicineWithSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  dietaryMadicineWithSwiftUIApp.swift
+//  dietaryMadicineWithSwiftUI
+//
+//  Created by bora on 2022/07/29.
+//
+
+import SwiftUI
+
+@main
+struct dietaryMadicineWithSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
