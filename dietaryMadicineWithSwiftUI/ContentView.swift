c9f1063e7c02500b240fab58825f567da7ba555e
bbora <>
2022-08-03 17:22:59 +0900

    2022.08.03 - 코드 수정

--- dietaryMadicineWithSwiftUI/ContentView.swift
+++ dietaryMadicineWithSwiftUI/ContentView.swift
@@ -8,67 +8,55 @@
 import SwiftUI
 
 struct ContentView: View {
-    
     var body: some View {
         VStack {
-            HStack {
-                Image("logo").frame(height: 60)
-                Spacer()
-            }.padding()
-            
-            HStack {
-                Text("당신을 위한 종합 \n영양 큐레이팅 서비스")
-                    .bold()
-                    .font(.system(size: 24))
-                Spacer()
-            }.padding()
-            
+            LogoView()
             Spacer().frame(height: 150)
             VStack {
-                Button(action: {
-                    print("애플")
-                }) {
-                    HStack(alignment: .center) {
-                        Image("apple")
-                        Spacer()
-                        Text("애플 로그인").bold().font(.system(size: 15))
-                        Spacer()
-                    }.padding()
-                }.frame(maxWidth: .infinity, maxHeight: 60)
-                    .background(.black)
-                    .foregroundColor(.white)
-                
-                Button(action: {
-                    print("카카오")
-                }) {
-                    HStack(alignment: .center) {
-                        Image("kakao").imageScale(.small)
-                        Spacer()
-                        Text("카카오 로그인").bold().font(.system(size: 15))
-                        Spacer()
-                    }.padding()
-                }.frame(maxWidth: .infinity, maxHeight: 60)
-                    .background(.yellow)
-                    .foregroundColor(.black)
-                
-                Button(action: {
-                    print("구글")
-                }) {
-                    HStack(alignment: .center) {
-                        Image("google").imageScale(.small)
-                        Spacer()
-                        Text("구글 로그인").bold().font(.system(size: 15))
-                        Spacer()
-                    }.padding()
-                }.frame(maxWidth: .infinity, maxHeight: 60)
-                    .background(.gray)
-                    .foregroundColor(.black)
-                Button("이메일 로그인") {
-                    print("이메일")
-                }.frame(maxWidth: .infinity, maxHeight: 60)
-                Spacer().frame(height: 20)
+                LoginButtonView(name: "애플 로그인", imgName: "apple", backColor: .black, textColor: .white)
+                LoginButtonView(name: "카카오 로그인", imgName: "kakao", backColor: .yellow, textColor: .black)
+                LoginButtonView(name: "구글 로그인", imgName: "google", backColor: .gray, textColor: .black)
+                LoginButtonView(name: "이메일 로그인", imgName: "", backColor: .white, textColor: .black)
             }
         }.frame(maxWidth: .infinity, maxHeight: .infinity).padding()
+    }
+}
+
+struct LogoView: View {
+    var body: some View {
+        HStack {
+            Image("logo").frame(height: 60)
+            Spacer()
+        }.padding()
+        
+        HStack {
+            Text("당신을 위한 종합 \n영양 큐레이팅 서비스")
+                .bold()
+                .font(.system(size: 24))
+            Spacer()
+        }.padding()
+    }
+}
+
+struct LoginButtonView: View {
+    let name: String
+    let imgName: String
+    let backColor: Color
+    let textColor: Color
+    
+    var body: some View {
+        Button(action: {
+            print("애플")
+        }) {
+            HStack(alignment: .center) {
+                Image(imgName)
+                Spacer()
+                Text(name).bold().font(.system(size: 15))
+                Spacer()
+            }.padding()
+        }.frame(maxWidth: .infinity, maxHeight: 60)
+            .background(backColor)
+            .foregroundColor(textColor)
     }
 }
 
